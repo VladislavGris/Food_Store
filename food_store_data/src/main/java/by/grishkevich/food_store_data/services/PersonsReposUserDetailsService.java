bc16de522e6dc0c64bc38332cb41784d6f749c4d
Vladislav Grishkevich <vladislav.grishkevich@gmail.com>
2021-12-26 00:53:21 +0300

    Security JWT. Exception handling for existing user

--- food_store_data/src/main/java/by/grishkevich/food_store_data/services/PersonsReposUserDetailsService.java
+++ food_store_data/src/main/java/by/grishkevich/food_store_data/services/PersonsReposUserDetailsService.java
@@ -1,35 +1,29 @@
 package by.grishkevich.food_store_data.services;
 
-import by.grishkevich.food_store_data.models.Administrator;
 import by.grishkevich.food_store_data.models.Client;
-import by.grishkevich.food_store_data.repositories.AdministratorRepository;
+import by.grishkevich.food_store_data.models.Person;
 import by.grishkevich.food_store_data.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonsReposUserDetailsService implements UserDetailsService {
 
     private ClientRepository clientRepo;
-    private AdministratorRepository adminRepo;
 
     @Autowired
-    public PersonsReposUserDetailsService(ClientRepository clientRepo, AdministratorRepository adminRepo){
+    public PersonsReposUserDetailsService(ClientRepository clientRepo){
         this.clientRepo = clientRepo;
-        this.adminRepo = adminRepo;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Administrator admin = adminRepo.findByEmail(email);
-        if(admin == null){
-            Client client = clientRepo.findByEmail(email);
-            if(client != null){
-                return client;
-            }
+    public Person loadUserByUsername(String email) throws UsernameNotFoundException {
+        Client client = clientRepo.findByEmail(email);
+        if(client != null){
             throw new UsernameNotFoundException("Email '" + email + "' not found");
         }
-        return admin;
+        return client;
     }
 }
